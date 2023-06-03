75ea6dccf5a3b92de73428ef4c0cdc11bf049d5c
Lukasz Szypulski <luk.szypulski@gmail.com>
2023-06-03 14:43:14 +0200

    [1] Initial spring configuration

---
+++ src/main/java/pl/wsb/bugtrucker/WsbBugtracker.java
@@ -0,0 +1,13 @@
+package pl.wsb.bugtrucker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbBugtracker {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsbBugtracker.class, args);
+	}
+
+}
